145a808f64e3fd71f54eeb94e5db504fc23509ee
joabson <joabson.vasconcelos@icloud.com>
2024-07-25 17:39:25 -0300

    update

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+import {RiShutDownLine} from 'react-icons/ri'
+import { Container, Profile, Logout } from "./styles";
+
+
+export function Header(){
+    return (
+        <Container>
+            <Profile>
+                <img 
+                    src="https://github.com/joabsonvasconcelos.png" 
+                    alt="Foto do usuário" 
+                />
+
+                <div>
+                    <span>Bem-Vindo</span>
+                    <strong>Joabson Vasconcelos</strong>
+                </div>
+            </Profile>
+
+            <Logout>
+                <RiShutDownLine />
+            </Logout>
+        </Container>
+    )
+};
