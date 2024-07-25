c3c3d4c74a0798040ac3c07994c60047a70176c4
joabson <joabson.vasconcelos@icloud.com>
2024-07-25 00:02:24 -0300

    update

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,18 @@
+import { Container } from './styles'
+
+import { Button } from '../../components/Button';
+
+export function Details(){
+  
+  return(
+    <Container>
+      <h1>Hello World</h1>
+      <span>Joabson Vasconcelos</span>
+
+      <Button title="Login" loading />
+      <Button title="Cadastrar"/>
+      <Button title="Voltar"/>
+    </Container>
+
+  )
+}
