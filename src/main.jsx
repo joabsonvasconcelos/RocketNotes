ae50a82185d09b5a5c0acfcb30d923667e260487
joabson <joabson.vasconcelos@icloud.com>
2024-08-01 20:30:41 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import GlobalStyles from './styles/global';
 import { ThemeProvider } from 'styled-components';
 
+import { MyContext } from './myContext';
+
 
 import theme from './styles/theme';
 
@@ -12,7 +14,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={ theme }>
       <GlobalStyles />
-        <Routes />
+        <MyContext>
+          <Routes />
+        </MyContext>
     </ThemeProvider>
   </React.StrictMode>,
 )
